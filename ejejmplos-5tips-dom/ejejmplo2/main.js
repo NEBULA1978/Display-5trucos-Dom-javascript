c156be292b935dc685da24c9df8bb36f6ec30239
NEBULA1978 <rpascualey@gmail.com>
2023-11-01 18:52:58 +0100

    Ejemplo 2 leer elelemntos html con javascript, los saltos de linea me pone #text undefined
    añado opcion para que no se vea y añado apunts al codigo

--- ejejmplos-5tips-dom/ejejmplo2/main.js
+++ ejejmplos-5tips-dom/ejejmplo2/main.js
@@ -1,10 +1,22 @@
 let elementInit = document.getElementById("firstElement");
 
-while(elementInit){
-    document.getElementById("textResult").innerHTML += ` <p>
+// Estamos recorriendo todos los elementos a través de sus hermanos
+while (elementInit) {
+    // Agregamos contenido al div con id="textResult"
+    document.getElementById("textResult").innerHTML += `<p>
         ${elementInit.nodeName}
         ${elementInit.innerHTML}
-    </p>`
+    </p>`;
+    
+    // Estas líneas permiten recorrer los elementos de manera correcta
+    // pero presentan algunos problemas, explicados a continuación:
 
-    elementInit = elementInit.nextSibling;
+    // Esta línea agrega texto (nodos de tipo texto "#text") y en algunos navegadores
+    // podría devolver "undefined" si el siguiente elemento es un salto de línea o espacio en blanco.
+    // elementInit = elementInit.nextSibling;
+    
+    // Para evitar mostrar "undefined" cuando existen saltos de línea o espacios en blanco,
+    // utilizamos "nextElementSibling" en lugar de "nextSibling".
+    // Esta alternativa solo recorre los elementos HTML, omitiendo los nodos de texto.
+    elementInit = elementInit.nextElementSibling;
 }
